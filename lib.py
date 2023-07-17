f1110e5a45a8262be58bf137a4da69ac9cc6ff97
Tian Zhang <tian.zhang@triflesoft.org>
2023-07-17 12:22:43 +0400

    pytest_absent_black_failed

--- lib.py
+++ lib.py
@@ -1,4 +1,4 @@
-# 13167168958216330921
+# 15073168958216310271
 class QuadraticEquationSolver:
     def __init__(self, a, b, c):
         self.a = a
@@ -6,15 +6,14 @@ class QuadraticEquationSolver:
         self.c = c
 
     def solve(self):
-        d = self.b**2 - 4 * self.a * self.c
+        d = self.b ** 2 - 4 * self.a * self.c
 
-        if d < 0:
-            return []
+        if d <= 0: return []
 
         if d == 0:
             return [-self.b / (2 * self.a)]
 
         return [
-            (-self.b + d**0.5) / (2 * self.a),
-            (-self.b - d**0.5) / (2 * self.a),
+            (-self.b + d ** 0.5) / (2 * self.a),
+            (-self.b - d ** 0.5) / (2 * self.a),
         ]
