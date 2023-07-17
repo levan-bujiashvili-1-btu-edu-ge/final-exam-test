6b17d1258526f078563eb11833055a41875efb6f
Tian Zhang <tian.zhang@triflesoft.org>
2023-07-17 12:22:37 +0400

    pytest_passed_black_passed

---
+++ lib.py
@@ -0,0 +1,20 @@
+# 9534168958215719378
+class QuadraticEquationSolver:
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def solve(self):
+        d = self.b**2 - 4 * self.a * self.c
+
+        if d < 0:
+            return []
+
+        if d == 0:
+            return [-self.b / (2 * self.a)]
+
+        return [
+            (-self.b + d**0.5) / (2 * self.a),
+            (-self.b - d**0.5) / (2 * self.a),
+        ]
